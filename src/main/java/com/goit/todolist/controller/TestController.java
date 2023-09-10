cb7d308a28bb7d44972f5e608eca299b093b5521
Dmytro Milovanov <milovanovu@gmail.com>
2023-09-10 17:45:41 +0200

    Created TestController, test.html

---
+++ src/main/java/com/goit/todolist/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.goit.todolist.controller;
+
+import ch.qos.logback.core.model.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+       return "test";
+    }
+
+
+}
